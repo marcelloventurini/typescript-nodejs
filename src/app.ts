7ece1bd0d14ca48e207e1a372a91c351ef339d56
Marcello Venturini <marcellofventurini@gmail.com>
2026-02-24 12:26:03 -0300

    refactor: migrate project from JavaScript to TypeScript

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express, { type Response } from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (_, res: Response) => {
+  res.send('curso typescript');
+});
+
+function createPet(
+  id: number,
+  name: string,
+  species: string,
+  age: number,
+  adopted: boolean,
+) {
+  return {
+    id,
+    name,
+    species,
+    age,
+    adopted,
+  };
+}
+
+let id = 0;
+function generateId() {
+  return (id += 1);
+}
+
+app.post('/pets', (_, res) => {
+  const pet1 = createPet(generateId(), 'katarina', 'cachorro', 7, true);
+  const pet2 = createPet(generateId(), 'mia', 'gato', 8, true);
+
+  res.send([pet1, pet2]);
+});
+
+export default app;
