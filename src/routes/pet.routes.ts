da2410f64d5fb921d2fb3d4f02c8de5be2999339
Marcello Venturini <marcellofventurini@gmail.com>
2026-03-02 17:18:24 -0300

    fix: add needed repositories in pet routes and create context for routes

--- src/routes/pet.routes.ts
+++ src/routes/pet.routes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import { AppDataSource } from '../config/data-source.js';
 import PetController from '../controller/pet.controller.js';
+import { Pet } from '../entities/pet.entity.js';
+import { PetRepositoryImpl } from '../repositories/pet.repository.impl.js';
 
-const petController = new PetController();
+const petRepository = new PetRepositoryImpl(AppDataSource.getRepository(Pet));
+const petController = new PetController(petRepository);
 const router = express.Router();
 
-router.post('/pets', petController.createPet);
-router.get('/pets', petController.getPets);
-router.put('/pets/:id', petController.updatePet);
-router.delete('/pets/:id', petController.deletePet);
+router.post('/pets', (req, res) => petController.createPet(req, res));
+router.get('/pets', (req, res) => petController.getPets(req, res));
+router.put('/pets/:id', (req, res) => petController.updatePet(req, res));
+router.delete('/pets/:id', (req, res) => petController.deletePet(req, res));
 
 export default router;
