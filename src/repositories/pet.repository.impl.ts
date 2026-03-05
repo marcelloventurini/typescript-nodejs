0b09ccf3fc8dd72556794776a1d73a9ee7cffd7c
Marcello Venturini <marcellofventurini@gmail.com>
2026-03-05 13:03:02 -0300

    feat: create getPetById method and implement updatePet method in repositories

--- src/repositories/pet.repository.impl.ts
+++ src/repositories/pet.repository.impl.ts
@@ -13,12 +13,16 @@ export class PetRepositoryImpl implements PetRepository {
     return await this.repository.find();
   }
 
+  async getPetById(id: number): Promise<Pet | null> {
+    return await this.repository.findOneBy({ id });
+  }
+
   async createPet(pet: Pet): Promise<void> {
     await this.repository.save(pet);
   }
 
   async updatePet(id: number, pet: Pet): Promise<void> {
-    throw new Error('Method not implemented.');
+    await this.repository.update(id, pet);
   }
 
   async deletePet(id: number): Promise<void> {
--- src/repositories/pet.repository.ts
+++ src/repositories/pet.repository.ts
@@ -2,6 +2,7 @@ import { Pet } from '../entities/pet.entity.js';
 
 export interface PetRepository {
   getPets(): Promise<Pet[]>;
+  getPetById(id: number): Promise<Pet | null>;
   createPet(pet: Pet): Promise<void>;
   updatePet(id: number, pet: Pet): Promise<void>;
   deletePet(id: number): Promise<void>;
