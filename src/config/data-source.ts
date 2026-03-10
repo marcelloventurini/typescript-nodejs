6790b56165afc041668c292519b1bf480e74c312
Marcello Venturini <marcellofventurini@gmail.com>
2026-03-10 16:24:18 -0300

    chore: register Adopter entity and router

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
+import { Adopter } from '../entities/adopter.entity.js';
 import { Pet } from '../entities/pet.entity.js';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: './src/config/database.sqlite',
-  entities: [Pet],
+  entities: [Pet, Adopter],
   synchronize: true,
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import petRouter from './pet.routes.js';
+import adopterRouter from './adopter.routes.js';
 
 const router = (app: express.Router) => {
-  app.use(express.json(), petRouter);
+  app.use(express.json(), petRouter, adopterRouter);
 };
 
 export default router;
